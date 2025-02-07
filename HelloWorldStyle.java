898911ac3ea16a3e8e5cbf8edc295289774ffa50
Valian Ijaola <valian.ijaola@stu.ocsb.ca>
2025-02-07 02:46:48 +0000

    added HelloWorldStyle.java

---
+++ HelloWorldStyle.java
@@ -0,0 +1,22 @@
+/**
+ * The HelloWorldStyle program implements an application that
+ * displays "Hello World! and one other message" to the standard output.
+ *
+ * <p>This class demonstrates proper JavaDoc formatting.</p>
+ *
+ * @author Val I
+ * @version 1.0
+ * @since 2025-02-06
+ */
+public class HelloWorldStyle {
+
+    /**
+     * The main method displays messages to the console.
+     *
+     * @param args Command-line arguments (not used).
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Alex,");
+        System.out.println("You are awesome!");
+    }
+}
